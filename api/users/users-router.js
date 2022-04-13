cfebe55e2cec03dd6b7f20bfae6a8db65e075e6e
Joseph Fantuzzi <dev.josephfantuzzi@gmail.com>
2022-04-13 16:37:30 -0400

    delete request in place

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -65,6 +65,26 @@ router.put("/:id", (req, res) => {
 router.delete("/:id", (req, res) => {
   // RETURN THE FRESHLY DELETED USER OBJECT
   // this needs a middleware to verify user id
+  const { id } = req.params;
+  let deletedUser;
+  UsersModel.getById(id)
+    .then((user) => {
+      deletedUser = user;
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "Error in retrieving the user from the database" });
+    });
+  UsersModel.remove(id)
+    .then((count) => {
+      if (count === 1) {
+        res.status(200).json(deletedUser);
+      } else {
+        res.status(404).json({ message: "The user with the specified id could not be found" });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "Error in deleting the user from the database" });
+    });
 });
 
 router.get("/:id/posts", (req, res) => {
